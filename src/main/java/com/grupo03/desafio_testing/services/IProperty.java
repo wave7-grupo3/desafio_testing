db41954afd423c96dfd775b0a2b3daad2d46bd31
Gabriel Morais <gabriel.mmorais@mercadolivre.com>
2022-10-13 17:00:52 -0300

    feat: implements createProperty in layers Repository, Service and Controller

---
+++ src/main/java/com/grupo03/desafio_testing/services/IProperty.java
@@ -0,0 +1,9 @@
+package com.grupo03.desafio_testing.services;
+
+import com.grupo03.desafio_testing.model.Property;
+
+import java.util.List;
+
+public interface IProperty {
+    List<Property> createProperty(Property property);
+}
---
+++ src/main/java/com/grupo03/desafio_testing/services/PropertyService.java
@@ -0,0 +1,20 @@
+package com.grupo03.desafio_testing.services;
+
+import com.grupo03.desafio_testing.model.Property;
+import com.grupo03.desafio_testing.repository.PropertyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PropertyService implements IProperty {
+
+    @Autowired
+    private PropertyRepository propertyRepository;
+
+    @Override
+    public List<Property> createProperty(Property property) {
+        return propertyRepository.createProperty(property);
+    }
+}
