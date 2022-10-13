0ad6aa2785bc01c476eefa304a110a67e9bb4297
Gabriel Morais <gabriel.mmorais@mercadolivre.com>
2022-10-13 15:22:48 -0300

    build: initial setup

---
+++ src/main/java/com/grupo03/desafio_testing/DesafioTestingApplication.java
@@ -0,0 +1,13 @@
+package com.grupo03.desafio_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grupo03/desafio_testing/DesafioTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo03.desafio_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
