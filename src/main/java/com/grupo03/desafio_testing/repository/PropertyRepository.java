f6244c7ed6d5a027e8ee14328e6e3e8437b2b739
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-14 14:38:04 -0300

    :rocket: feat: implements verifyDistrictExist in PropertyRepository

--- src/main/java/com/grupo03/desafio_testing/repository/PropertyRepository.java
+++ src/main/java/com/grupo03/desafio_testing/repository/PropertyRepository.java
@@ -1,27 +1,45 @@
 package com.grupo03.desafio_testing.repository;
 
-import com.fasterxml.jackson.databind.annotation.JsonAppend;
+import com.grupo03.desafio_testing.advisor.NotFoundException;
+import com.grupo03.desafio_testing.model.District;
 import com.grupo03.desafio_testing.model.Property;
 import com.grupo03.desafio_testing.model.Room;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Repository;
+
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
-@NoArgsConstructor
 public class PropertyRepository {
 
     private final List<Property> properties = new ArrayList<>();
 
+    private final List<District> districts = new ArrayList<>();
+
+    public PropertyRepository() {
+        districts.add(new District("Santa Catarina", BigDecimal.valueOf(8000)));
+        districts.add(new District("Curitiba", BigDecimal.valueOf(6000)));
+        districts.add(new District("Brasília", BigDecimal.valueOf(9000)));
+        districts.add(new District("Belo Horizonte", BigDecimal.valueOf(5000)));
+        districts.add(new District("São Paulo", BigDecimal.valueOf(10000)));
+    }
+
     public Property createProperty(Property property) {
-        calculateTotalRoomArea(property);
-        calculateTotalPropArea(property);
-        calculateTotalPropPrice(property);
-        properties.add(property);
-        return property;
+        List<District> listDistricts = verifyDistrictExist(property.getPropDistrict());
+
+        if(!listDistricts.isEmpty()) {
+            property.getPropDistrict().setValueDistrictM2(listDistricts.get(0).getValueDistrictM2());
+            calculateTotalRoomArea(property);
+            calculateTotalPropArea(property);
+            calculateTotalPropPrice(property);
+            properties.add(property);
+            return property;
+        } else {
+            throw new NotFoundException("District not found!");
+        }
     }
 
     public List<Property> getAll() {
@@ -42,5 +60,11 @@ public class PropertyRepository {
     private void calculateTotalPropPrice(Property property) {
         BigDecimal totalPrice = BigDecimal.valueOf(property.getTotalPropArea()).multiply(property.getPropDistrict().getValueDistrictM2());
         property.setTotalPropPrice(totalPrice);
+    }
+
+    private List<District> verifyDistrictExist(District district) {
+        return districts.stream()
+                .filter(dist -> dist.getDistrictName().equals(district.getDistrictName()))
+                .collect(Collectors.toList());
     }
 }
