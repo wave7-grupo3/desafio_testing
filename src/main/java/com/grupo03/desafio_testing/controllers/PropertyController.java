a910d3a93d19e1dbb959e0284452b34376dbf473
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-17 18:00:27 -0300

    :books: docs: implements javaDocs in layer Controller

--- src/main/java/com/grupo03/desafio_testing/controllers/PropertyController.java
+++ src/main/java/com/grupo03/desafio_testing/controllers/PropertyController.java
@@ -17,17 +17,34 @@ public class PropertyController {
 
     @Autowired
     private IProperty propertyService;
-    
+
+    /**
+     * Rota responsável pela criação de uma propriedade.
+     * @author Grupo 03
+     * @param property - Property
+     * @return Property - Retorna uma entidade do tipo Property.
+     */
     @PostMapping
     public ResponseEntity<Property> createProperty(@Valid @RequestBody Property property) {
         return new ResponseEntity<>(propertyService.createProperty(property), HttpStatus.CREATED);
     }
 
+    /**
+     * Rota responsável por listar todas as propriedades.
+     * @author Grupo 03
+     * @return List<Property> - Retorna uma lista de Propriedades.
+     */
     @GetMapping()
     public ResponseEntity<List<Property>> getAll() {
         return new ResponseEntity<>(propertyService.getAll(), HttpStatus.OK);
     }
 
+    /**
+     * Rota responsável por retornar o maior comodo de uma propriedade.
+     * @author Grupo 03
+     * @param id - String
+     * @return Room - Retorna uma entidade do tipo Room.
+     */
     @GetMapping("/room/{id}")
     public ResponseEntity<Room> getBiggestRoom(@PathVariable String id) {
         return new ResponseEntity<>(propertyService.getBiggestRoom(id), HttpStatus.OK);
